66ddf12577c1bc7f8a11862d111e615d80fb49d1
Mae Koger <mkoger24@wooster.edu>
2023-02-07 16:21:10 -0500

    getting started

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from Flask import Flask, render_template, request
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='templates', static_folder='css')
 
-@app.route('/', methods='GET')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template(home.html)
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host='localhost')
