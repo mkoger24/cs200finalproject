26fa01f1ecad31cf84960748941a6b7a05644183
Mae Koger <mkoger24@wooster.edu>
2023-04-05 12:21:22 -0400

    download pdf upon submit

--- app.py
+++ app.py
@@ -2,14 +2,19 @@ from flask import Flask, render_template, request, send_file
 
 app = Flask(__name__, template_folder='templates', static_folder='css')
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def home():
     return render_template("home.html")
 
-@app.route('/download')
+@app.route('/', methods=['POST'])
+def input():
+    return render_template("input-info.php")
+
+
+@app.route('/download', methods=['GET', 'POST'])
 def downloadFile ():
     #For windows you need to use drive name [ex: F:/Example.pdf]
-    path = "./templates/patternpdfs/bernasconi2007.pdf"
+    path = "./templates/patternpdfs/output.pdf"
     return send_file(path, as_attachment=True)
 
 if __name__ == "__main__":
