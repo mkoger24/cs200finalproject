a54f1175025c9c2a3f69aa8103fd58781155b4bd
Mae Koger <mkoger24@wooster.edu>
2023-04-07 10:32:46 -0400

    functionality to create text file

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, send_file
 
 app = Flask(__name__, template_folder='templates', static_folder='css')
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET','POST'])
 def home():
     return render_template("home.html")
 
@@ -13,8 +13,9 @@ def input():
 
 @app.route('/download', methods=['GET', 'POST'])
 def downloadFile ():
-    #For windows you need to use drive name [ex: F:/Example.pdf]
-    path = "./templates/patternpdfs/output.pdf"
+    with open('templates/patternpdfs/output.txt', 'w') as outputFile:
+        outputFile.write('Create a new text file!')
+    path = "./templates/patternpdfs/output.txt"
     return send_file(path, as_attachment=True)
 
 if __name__ == "__main__":
